71e3b8a04ee114d74b637c4567f769f867aae154
diogomreiras <diogo.mr.eiras@tutanota.com>
2025-11-02 17:34:36 +0100

    Link the repo and the webpage

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # e-plantShopping
 
 Project cloned from https://github.com/ibm-developer-skills-network/e-plantShopping?tab=Apache-2.0-1-ov-file#readme, under the  Apache-2.0 license, included here unmodified for reference.
+
+
+## Website
+
+https://diogomreiras.github.io/e-plantshopping/
--- src/AboutUs.jsx
+++ src/AboutUs.jsx
@@ -3,13 +3,17 @@ import './AboutUs.css';
 function AboutUs() {
   return (
     <div className="about-us-container">
-      <p className="about-us-description">Welcome to Paradise Nursery, where green meets serenity!</p>
+      <p className="about-us-description">
+        Welcome to Paradise Nursery, where green meets serenity!
+      </p>
+
       <p className="about-us-content">
         At Paradise Nursery, we are passionate about bringing nature closer to you. Our mission is to provide a wide range of
         high-quality plants that not only enhance the beauty of your surroundings but also contribute to a healthier and
         more sustainable lifestyle. From air-purifying plants to aromatic fragrant ones, we have something for every
         plant enthusiast.
       </p>
+
       <p className="about-us-content">
         Our team of experts is dedicated to ensuring that each plant meets our strict standards of quality and care.
         Whether you're a seasoned gardener or just starting your green journey, we're here to support you every step of
@@ -20,6 +24,14 @@ function AboutUs() {
       <p className="about-us-content">
         Join us in our mission to create a greener, healthier world. Visit Paradise Nursery today and experience the
         beauty of nature right at your doorstep.
+      </p>
+
+      <br />
+      <br />
+
+      <p className="about-us-content">
+        This is a learning project. <br />
+        Check our source code at <a href="https://github.com/diogomreiras/e-plantshopping" style={{ color: "white" }}>https://github.com/diogomreiras/e-plantshopping</a>
       </p>
     </div>
   );
